272908ee4585a7c123db2009fe8657e6487d4e35
Huihao Chen <hchen106@ucsc.edu>
2020-12-18 14:24:51 -0800

    fixed layouts of safari

--- src/pages/About.js
+++ src/pages/About.js
@@ -22,57 +22,50 @@ function About() {
     return (
         <div className = 'about'>
            <div className = 'row'>
-                
-                    <div className = 'about-top-desc-wrapper'>
-                        <div className = 'about-top-desc-container'>
-                            <h4 className = 'about-title'>About me</h4>
-                            <p className = 'about-desc'>
-                            I am a recent graduate of 2020 from University of California, Santa Cruz with a B.S.
-                            in computer engineering who enjoys using my skills and thought to build professional 
-                            and interesting applications and projects.     
-                            </p>
-                        </div>
-                        <div className = 'about-top-pic-container'>
-                            <img className = 'top-pic' src = {profile_picture}></img>
-                        </div>
-                        
-                    </div>
-                
                
-                    <div className = 'about-bottom-info-wrapper'>
-                       
-                        <div className = 'bottom-contact-container'>
-                            <div className = 'skill-container'>
-                                <h5 className = 'skill-header'>My Skills</h5>
-                                <ul className = 'skill-list'>
-                                    {SkillData.map((item, index)  => {
-                                        return <li key = {index} className = {item.cName}>
-                                            <div className = "skill-item-container">
-                                                    <div className ='skill-icon'>
-                                                        {item.icon}
-                                                    </div>
-                                                    <div className = 'skill-name'>
-                                                        {item.Name}
-                                                    </div>
-                                            </div>
-                                            
-                                        </li>
-                                    })}
-                                </ul>
-                            </div>
-                            <div className = 'bottom-contact-icons'>
-                            
-                                    <CopyToClipboard text={"852165175andy@gmail.com"}>
-                                        <AiOutlineMail className = 'bottom-contact-icon' onClick={copy}/>
-                                    </CopyToClipboard>
-                                    <AiFillLinkedin className = 'bottom-contact-icon' onClick={()=> window.open("https://www.linkedin.com/in/huihao-chen/", "_blank")} />
-                                    <AiFillGithub className = 'bottom-contact-icon' onClick={()=> window.open("https://github.com/hchen106", "_blank")}/>
-                                    <button className = 'bottom-contact-button' onClick={()=> window.open("https://drive.google.com/file/d/17duJNkHHiIRI4nnfwwIQhmh3rY2ojVBe/view?usp=sharing", "_blank")}>Resume</button>
-                                   
-                            </div>
-                        </div>
-                       
+                <div className = 'about-top-desc-wrapper'>
+                    <div className = 'about-top-desc-container'>
+                        <h4 className = 'about-title'>About me</h4>
+                        <p className = 'about-desc'>
+                        I am a recent graduate of 2020 from University of California, Santa Cruz with a B.S.
+                        in computer engineering who enjoys using my skills and thought to build professional 
+                        and interesting applications and projects.     
+                        </p>
                     </div>
+                     <img className = 'top-pic' src = {profile_picture}></img>
+                </div> 
+               
+                <div className = 'bottom-contact-container'>
+                    <div className = 'skill-container'>
+                        <h5 className = 'skill-header'>My Skills</h5>
+                        <ul className = 'skill-list'>
+                            {SkillData.map((item, index)  => {
+                                return <li key = {index} className = {item.cName}>
+                                    <div className = "skill-item-container">
+                                            <div className ='skill-icon'>
+                                                {item.icon}
+                                            </div>
+                                            <div className = 'skill-name'>
+                                                {item.Name}
+                                            </div>
+                                    </div>
+                                </li>
+                            })}
+                        </ul>
+                    </div>
+                    <div className = 'bottom-contact-icons'>
+                    
+                            <CopyToClipboard text={"852165175andy@gmail.com"}>
+                                <AiOutlineMail className = 'bottom-contact-icon' onClick={copy}/>
+                            </CopyToClipboard>
+                            <AiFillLinkedin className = 'bottom-contact-icon' onClick={()=> window.open("https://www.linkedin.com/in/huihao-chen/", "_blank")} />
+                            <AiFillGithub className = 'bottom-contact-icon' onClick={()=> window.open("https://github.com/hchen106", "_blank")}/>
+                            <button className = 'bottom-contact-button' onClick={()=> window.open("https://drive.google.com/file/d/17duJNkHHiIRI4nnfwwIQhmh3rY2ojVBe/view?usp=sharing", "_blank")}>Resume</button>
+
+                    </div>
+                </div>
+                    
+               
                
            </div>
            
